a2488c83ff174f48ef28e49584f24dccd8d3e5ef
Varun Khalate <khalatevarun@gmail.com>
2021-10-09 23:13:01 +0530

    add: color indication for each correct and wrong answer entered | new quote generation on correct answer

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
 
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+  const arrayValue = quoteInputElement.value.split('');
+  let correct = true;
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index];
+    if (character == null) {
+      characterSpan.classList.remove('correct');
+      characterSpan.classList.remove('incorrect');
+      correct = false;
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct');
+      characterSpan.classList.remove('incorrect');
+    } else {
+      characterSpan.classList.add('incorrect');
+      characterSpan.classList.remove('correct');
+      correct = false;
+    }
+  });
+  if (correct) renderNewQuote();
+});
+
 function getRandomQuote() {
   return fetch(RANDOM_QUOTE_API_URL)
     .then((response) => response.json())
@@ -15,10 +37,9 @@ async function getNextQuote() {
 
 async function renderNewQuote() {
   const quote = await getRandomQuote();
-  quoteDisplayElement.innerText = quote;
+  quoteDisplayElement.innerHTML = '';
   quote.split('').forEach((character) => {
     const charaterSpan = document.createElement('span');
-    charaterSpan.classList.add('correct');
     charaterSpan.innerText = character;
     quoteDisplayElement.appendChild(charaterSpan);
   });
