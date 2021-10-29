c3926c7588b8aca213454b60e3223da561038b30
Varun Khalate <khalatevarun@gmail.com>
2021-10-29 16:36:53 +0530

    add: other stats and table to display the stats

--- script.js
+++ script.js
@@ -2,23 +2,42 @@ const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
 const timerElement = document.getElementById('timer');
+const wpmElement = document.getElementById('wpm');
+const cpmElement = document.getElementById('cpm');
+const errorsElement = document.getElementById('errors');
+const accuracyElement = document.getElementById('accuracy');
+const totalwordsElement = document.getElementById('totalwords');
+
 let wpm = 0;
 let totalCharacters = 0;
 let correctCharacters = 0;
 let cpm = 0;
 let startTime;
+let duration = 0;
 let quoteIndex = 0;
 let currentQuote = [];
+let errors = 0;
+let mode = 1;
+let totalWords = 0;
+let accuracy = 0;
 
-quoteInputElement.addEventListener('input', () => {
+const records = [];
+const singleRecord = {
+  wpm: 0,
+  cpm: 0,
+  duration: 0,
+  words: 0,
+  errors: 0,
+  accuracy: 0,
+};
+
+quoteInputElement.addEventListener('input', (e) => {
   console.log('Characters typed is>>>>>', totalCharacters);
   const arrayQuote = quoteDisplayElement.querySelectorAll('span');
   const arrayValue = quoteInputElement.value.split('');
   let correct = true;
   arrayQuote.forEach((characterSpan, index) => {
     const character = arrayValue[index];
-
-    console.log('IN FOREACH');
 
     if (character == null) {
       characterSpan.classList.remove('correct');
@@ -27,14 +46,22 @@ quoteInputElement.addEventListener('input', () => {
     } else if (character === characterSpan.innerText) {
       characterSpan.classList.add('correct');
       characterSpan.classList.remove('incorrect');
-      correctCharacters++;
     } else {
       characterSpan.classList.add('incorrect');
       characterSpan.classList.remove('correct');
       correct = false;
-      correctCharacters--;
     }
   });
+
+  totalCharacters++;
+  console.log('KEY PRSSED>>', e);
+  if (currentQuote[quoteIndex] === e.data) {
+    correctCharacters++;
+    quoteIndex++;
+  } else {
+    quoteInputElement.value = quoteInputElement.value.slice(0, -1);
+    errors++;
+  }
 
   cpm =
     correctCharacters > 5
@@ -42,8 +69,25 @@ quoteInputElement.addEventListener('input', () => {
       : 0;
 
   wpm = Math.round(cpm / 5);
+  totalWords = Math.floor(correctCharacters / 5);
+  accuracy =
+    Math.round(
+      ((correctCharacters / totalCharacters) * 100 + Number.EPSILON) * 100
+    ) / 100;
 
-  console.log('QUOTE INDEX>?>>', quoteIndex);
+  console.log('WPM>>>', wpm);
+  wpmElement.innerText = wpm;
+  cpmElement.innerText = cpm;
+  errorsElement.innerText = errors;
+  accuracyElement.innerText = `${
+    Math.round(
+      ((correctCharacters / totalCharacters) * 100 + Number.EPSILON) * 100
+    ) / 100
+  }%`;
+  totalwordsElement.innerText = Math.floor(correctCharacters / 5);
+  console.log('CPM>>>', cpm);
+  console.log('Correct characters', correctCharacters);
+  console.log('Current time>>>', getTimerTime());
 
   if (correct) renderNewQuote();
 });
@@ -55,23 +99,53 @@ function getRandomQuote() {
 }
 
 quoteInputElement.addEventListener('keydown', function (e) {
-  console.log('ARRAY QUOTE INDEX>>>', currentQuote[quoteIndex]);
   /*
    * keyCode: 8
    * keyIdentifier: "U+0008"
    */
-  console.log('EVent>>', e);
+
   if (e.key === 'Backspace') {
-    console.log('Backpressed');
-    totalCharacters--;
-    quoteIndex--;
-  } else {
-    totalCharacters++;
-    if (currentQuote[quoteIndex] === e.key) {
-      quoteIndex++;
-    }
+    e.preventDefault();
   }
+  if (e.key === 'Escape') {
+    stopTest();
+  }
+  console.log('key pressed>>>', e);
 });
+
+function stopTest() {
+  // stop only if one minute is passed else show alert
+  //first add the current stats to an array of objects to display it later in the stats
+  // reset everything to 0 and show on the website
+  if (duration > 1) {
+    records.push({
+      wpm: wpm,
+      cpm: cpm,
+      duration: duration,
+      words: totalWords,
+      errors: errors,
+      accuracy: accuracy,
+    });
+
+    wpm = 0;
+    cpm = 0;
+    duration = 0;
+    words = 0;
+    errors = 0;
+    accuracy = 0;
+    timerElement.innerText = 0;
+    wpmElement.innerText = wpm;
+    cpmElement.innerText = cpm;
+    errorsElement.innerText = errors;
+    accuracyElement.innerText = accuracy;
+    totalwordsElement.innerText = words;
+  }
+  console.log('RECORDS ARES>>>>>>', records);
+}
+
+function restartTest() {
+  // reset everything to 0 and do not add these stats to the records
+}
 
 async function getNextQuote() {
   const quote = await getRandomQuote();
@@ -79,28 +153,33 @@ async function getNextQuote() {
 }
 
 async function renderNewQuote() {
-  const quote = await getRandomQuote();
+  quoteIndex = 0;
+  let quote = await getRandomQuote();
   currentQuote = quote.split('');
   quoteDisplayElement.innerHTML = '';
+  if (mode === 1) {
+    quote = quote.toLowerCase().replace(/[^\w\s]/gi, '');
+    currentQuote = quote.split('');
+  }
   quote.split('').forEach((character) => {
     const charaterSpan = document.createElement('span');
     charaterSpan.innerText = character;
     quoteDisplayElement.appendChild(charaterSpan);
   });
   quoteInputElement.value = null;
-  startTimer();
 }
 
 function startTimer() {
   timerElement.innerText = 0;
   startTime = new Date();
   setInterval(() => {
-    timer.innerText = getTimerTime();
+    timer.innerText = `${getTimerTime()}s`;
   }, 1000);
 }
 
 function getTimerTime() {
-  return Math.floor((new Date() - startTime) / 1000);
+  duration = Math.floor((new Date() - startTime) / 1000);
+  return duration;
 }
-
+startTimer();
 renderNewQuote();
